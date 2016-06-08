57a3ea79f195710b5fcf3fcb304bb239567d8d40
Gio d'Amelio <giodamelio@gmail.com>
2016-06-08 09:47:51 -0700

    Return students from express server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 
+const CanvasAPI = require('./canvas');
+
+const canvas = new CanvasAPI(process.env.CANVAS_TOKEN);
 const server = express();
 
 server.get('/', function(req, res) {
-  res.send('Hello World');
+  canvas.getCourses()
+    .then(function(courses) {
+      return Promise.all(courses.map(function(course) {
+        return canvas.getStudents(course.id);
+      }));
+    })
+    .then(function(students) {
+      res.send(students);
+    })
+    .catch(function(error) {
+      console.error(error);
+      res.status(500);
+      res.send({
+        error: error.message,
+      });
+    });
 });
 
 const PORT = process.env.PORT || 3141;
