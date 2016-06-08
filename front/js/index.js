e89e1e742a48cc3d02c6e40cc2fd542bf7a35e10
Gio d'Amelio <giodamelio@gmail.com>
2016-06-08 11:33:30 -0700

    React hello world

--- front/js/index.js
+++ front/js/index.js
@@ -1,1 +1,13 @@
-console.log(`1 + 100 = ${1 + 100}`);
+const React = require('react');
+const reactDom = require('react-dom');
+
+class HelloWorld extends React.Component {
+  render() {
+    return <h1>Hello React!</h1>;
+  }
+}
+
+reactDom.render(
+  <HelloWorld></HelloWorld>,
+  document.getElementById('app')
+);
