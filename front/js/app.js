2c42aedbddbca60ce5cfdbfb51e71181e5cab191
Gio d'Amelio <giodamelio@gmail.com>
2016-06-09 17:07:34 -0700

    Add react-router-bootstrap

--- front/js/app.js
+++ front/js/app.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Student Pairs</h1>
-        <Link to="/">Home</Link>
-        <Link to="/courses">Courses</Link>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Student Pairs</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <IndexLinkContainer to="/">
+                <NavItem eventKey={1} href="#">Home</NavItem>
+              </IndexLinkContainer>
+              <LinkContainer to="/courses">
+                <NavItem eventKey={2} href="#">Courses</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
         {this.props.children}
       </div>
